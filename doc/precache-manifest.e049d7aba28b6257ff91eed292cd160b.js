ee8fb1351f9d0a66cb0b7130b0bd15a2c2941f9b
Faezeh Amri <amrifaezeh@gmail.com>
2019-09-05 03:31:06 +0800

    test github page with doc

---
+++ doc/precache-manifest.e049d7aba28b6257ff91eed292cd160b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8414cd02a4013fb95cf8fc3a1e5f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15514bebcdd57b6410a7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1dfba51a24e550f0762f",
+    "url": "/static/js/2.e48a22b0.chunk.js"
+  },
+  {
+    "revision": "15514bebcdd57b6410a7",
+    "url": "/static/js/main.e12f0b5d.chunk.js"
+  },
+  {
+    "revision": "e2115a0240984fb5373f",
+    "url": "/static/js/runtime~main.caff8658.js"
+  }
+]);

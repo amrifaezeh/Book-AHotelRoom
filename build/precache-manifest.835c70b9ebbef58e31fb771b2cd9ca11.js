4ace65abc7828d5dde86deed96489d7f7687c2f8
Faezeh Amri <amrifaezeh@gmail.com>
2019-09-07 20:14:06 +0800

    gh-page

---
+++ build/precache-manifest.835c70b9ebbef58e31fb771b2cd9ca11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124455ac4252bbce7039216868a0f8fa",
+    "url": "/Book-AHotelRoom/index.html"
+  },
+  {
+    "revision": "ee99df22f5a3dde76eea",
+    "url": "/Book-AHotelRoom/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2399c84db3903b93eeaf",
+    "url": "/Book-AHotelRoom/static/js/2.043d740c.chunk.js"
+  },
+  {
+    "revision": "ee99df22f5a3dde76eea",
+    "url": "/Book-AHotelRoom/static/js/main.ce54f0bd.chunk.js"
+  },
+  {
+    "revision": "b280e319abddeb8f8d24",
+    "url": "/Book-AHotelRoom/static/js/runtime~main.7d2ca89b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e049d7aba28b6257ff91eed292cd160b.js"
+  "/Book-AHotelRoom/precache-manifest.835c70b9ebbef58e31fb771b2cd9ca11.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Book-AHotelRoom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

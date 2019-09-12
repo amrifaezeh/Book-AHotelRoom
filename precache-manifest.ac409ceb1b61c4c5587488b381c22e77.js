dc3bc4a9e8b67f51ab3b2746f6259ddeae814879
Faezeh Amri <amrifaezeh@gmail.com>
2019-09-12 12:22:22 +0800

    Updates

--- precache-manifest.ac409ceb1b61c4c5587488b381c22e77.js
+++ precache-manifest.ac409ceb1b61c4c5587488b381c22e77.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb3401f4cf8475360c62ed4f20f4d322",
+    "revision": "6defcec8bfbd5071f54e983ddbc2211a",
     "url": "/Book-AHotelRoom/index.html"
   },
   {
-    "revision": "f08e5ba717a525d7841b",
-    "url": "/Book-AHotelRoom/static/css/main.6e83550e.chunk.css"
+    "revision": "34668961c27f63d3e863",
+    "url": "/Book-AHotelRoom/static/css/main.64b0cf91.chunk.css"
   },
   {
     "revision": "4a75475c02a62917a8cc",
     "url": "/Book-AHotelRoom/static/js/2.36058545.chunk.js"
   },
   {
-    "revision": "f08e5ba717a525d7841b",
-    "url": "/Book-AHotelRoom/static/js/main.f909d660.chunk.js"
+    "revision": "34668961c27f63d3e863",
+    "url": "/Book-AHotelRoom/static/js/main.26f7185b.chunk.js"
   },
   {
     "revision": "fcd0bba94fca21590bd8",
